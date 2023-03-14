f104b690b4a39327916d9b9930d23ec185d0a72d
Toaster5 <105885246+ToastL@users.noreply.github.com>
2023-03-14 14:46:10 +0100

    Add files via upload

---
+++ routes/index.tsx
@@ -0,0 +1,43 @@
+import { asset, Head } from "$fresh/runtime.ts";
+import plan from "../static/etien.json" assert { type: "json" };
+
+export default function Home() {
+  const date = new Date()
+
+  const yearsInDays = (date.getFullYear()-2000)*365
+  const days = Math.ceil((date - new Date(date.getFullYear(),0,1))/ 86400000)
+
+  const daysSince2000 = yearsInDays+days+3
+
+  const today = plan.days[daysSince2000 % plan.days.length]
+  const tomorrow = plan.days[(daysSince2000+1) % plan.days.length]
+  const todaysWorkout = (today != "rest") ? plan.workouts[daysSince2000 % plan.days.length] : []
+
+  const currClass = (today == "rest") ? "wrong" : "correct"
+
+  const planning: Array<preact.JSX.Element> = [];
+
+  todaysWorkout.forEach((workout: string) => {
+    planning.push(<li>{workout}</li>)
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+        <link rel="stylesheet" href={asset("styles.css")} />
+      </Head>
+      <div className={"main " + currClass}>
+        <div className="container mainContainer">
+          <h1>{today}</h1>
+          {todaysWorkout.length != 0 &&
+            <li>{planning}</li>
+          }
+        </div>
+        <div className="container">
+          <h3>Tomorrow: {tomorrow}</h3>
+        </div>
+      </div>
+    </>
+  );
+}
